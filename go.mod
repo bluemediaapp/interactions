4e7d2c33e9e4d331273a888c2cdf81beb5c4e5da
Tag-Epic <tagepicuwu@gmail.com>
2021-04-06 15:04:29 +0200

    Feat: Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module interactions
+
+go 1.16
+
+require (
+	github.com/bluemediaapp/models v0.0.0-20210406115354-79ede849b945 // indirect
+	github.com/gofiber/fiber/v2 v2.7.1 // indirect
+	go.mongodb.org/mongo-driver v1.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"context"
+	"github.com/bluemediaapp/models"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+var (
+	app    = fiber.New()
+	client *mongo.Client
+	config *Config
+
+	mctx = context.Background()
+
+	videosCollection *mongo.Collection
+	likedVideosCollection *mongo.Collection
+	usersCollection *mongo.Collection
+
+)
+
+func main() {
+	config = &Config{
+		port:     os.Getenv("port"),
+		mongoUri: os.Getenv("mongo_uri"),
+	}
+
+	app.Post("/like/:video_id/:user_id", func(ctx *fiber.Ctx) error {
+		userId, err := strconv.ParseInt(ctx.Params("user_id"), 10, 64)
+		if err != nil {
+			return err
+		}
+		videoId, err := strconv.ParseInt(ctx.Params("video_id"), 10, 64)
+		if err != nil {
+			return err
+		}
+
+		if hasLiked(userId, videoId) {
+			_ = ctx.SendStatus(412)
+			_ = ctx.SendString("User has already liked this post")
+			return nil
+		}
+
+		user, err := getUser(userId)
+		if err != nil {
+			return err
+		}
+
+		video, err := getVideo(videoId)
+		if err != nil {
+			return err
+		}
+
+		err = likeVideo(user, video)
+
+
+		return err
+	})
+
+	initDb()
+	log.Fatal(app.Listen(config.port))
+}
+
+func initDb() {
+	// Connect mongo
+	var err error
+	client, err = mongo.NewClient(options.Client().ApplyURI(config.mongoUri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(mctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Setup tables
+	db := client.Database("blue")
+	videosCollection = db.Collection("video_metadata")
+	likedVideosCollection = db.Collection("liked_videos")
+	usersCollection = db.Collection("users")
+}
+
+func hasLiked(userId int64, videoId int64) bool {
+	filter := bson.D{{"user_id", userId}, {"video_id", videoId}}
+	var limit int64 = 1
+	documentCount, err := likedVideosCollection.CountDocuments(mctx, filter, &options.CountOptions{
+		Limit: &limit,
+	})
+	if err != nil {
+		log.Print(err)
+		return true
+	}
+	return documentCount == int64(1)
+}
+func likeVideo(user models.DatabaseUser, video models.DatabaseVideo) error {
+	// Duplicate checks
+	likeEvent := models.DatabaseLikeEvent{
+		VideoId: video.Id,
+		UserId:  user.Id,
+	}
+	_, err := likedVideosCollection.InsertOne(mctx, likeEvent)
+	if err != nil {
+		return err
+	}
+
+	// Interests
+	for _, tag := range video.Tags {
+		currentInterestValue, exists := user.Interests[tag]
+		if !exists {
+			currentInterestValue = 0
+		}
+		currentInterestValue += 1
+		user.Interests[tag] = currentInterestValue
+	}
+	update := bson.D{{"$set", bson.D{{"interests", user.Interests}}}}
+	filter := bson.D{{"_id", user.Id}}
+
+	_, err = usersCollection.UpdateOne(mctx, filter, update)
+	if err != nil {
+		return err
+	}
+
+
+	// Like count
+	if video.Likes >= math.MaxInt64 - 1 {
+		log.Printf("Max likes on video %d", video.Id)
+		return err
+	}
+	_, err = videosCollection.UpdateOne(mctx, bson.D{{"_id", video.Id}}, bson.D{{"$inc", bson.D{{"likes", 1}}}})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getUser(userId int64) (models.DatabaseUser, error) {
+	query := bson.D{{"_id", userId}}
+	rawUser := usersCollection.FindOne(mctx, query)
+	var user models.DatabaseUser
+	err := rawUser.Decode(&user)
+	if err != nil {
+		return models.DatabaseUser{}, err
+	}
+	return user, nil
+}
+
+func getVideo(videoId int64) (models.DatabaseVideo, error) {
+	query := bson.D{{"_id", videoId}}
+	rawVideo := videosCollection.FindOne(mctx, query)
+	var video models.DatabaseVideo
+	err := rawVideo.Decode(&video)
+	if err != nil {
+		return models.DatabaseVideo{}, err
+	}
+	return video, nil
+}
